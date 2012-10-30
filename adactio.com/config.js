d5c067cec7ad581302451c489fcb8b9bc3b4f99d
Sean Hogan <shogun70@gmail.com>
2012-10-30 21:02:59 +1100

    adactio.com config is working

--- adactio.com/config.js
+++ adactio.com/config.js
@@ -1,7 +1,6 @@
 (function() {
 
-var $ = function(selector, node) { return node.querySelector(selector); }
-var $$ = function(selector, node) { return node.querySelectorAll(selector); }
+var DOM = Meeko.DOM, $ = DOM.$, $$ = DOM.$$;
 
 Meeko.stuff.extend(Meeko.panner.options, {
 	detectView: function(path) {
@@ -18,8 +17,9 @@ Meeko.stuff.extend(Meeko.panner.options, {
 				var main = $("[role=main]", doc);
 				var skin = $("#skinselection", main);
 				if (skin) {
-					main.removeChild(skin.previousElementSibling);
-					main.removeChild(skin.nextElementSibling);
+					main.removeChild(skin.previousElementSibling); // <h3>Customise</h3>
+					main.removeChild(skin.nextElementSibling); // <p>This is a theme description</p>
+					main.removeChild(skin.nextElementSibling); // <p>Accessibility statement</p>
 					main.removeChild(skin);
 				}
 				main.parentNode.removeChild(main);
