3ec24d66e589a4f116c94c22cfe3ecdc7b347075
Sean Hogan <shogun70@gmail.com>
2012-11-01 14:29:21 +1100

    Magazine style site

---
+++ m.netmagazine.com/config.js
@@ -0,0 +1,30 @@
+(function() {
+
+var DOM = Meeko.DOM, $ = DOM.$, $$ = DOM.$$;
+
+Meeko.stuff.extend(Meeko.panner.options, {
+	detectView: function(path) {
+		return "default";
+	},
+	views: {
+		"default": {
+			decor: "decor.html",
+			preprocess: function(doc, path) {
+				$$('link[rel=stylesheet]', doc).forEach(function(node) { node.parentNode.removeChild(node); })
+				$$('script', doc).forEach(function(node) { node.parentNode.removeChild(node); });
+				$$('.bodyCopy', doc).forEach(function(node) { node.className = 'copy'; });
+				$$('.synopsis', doc).forEach(function(node) { node.className = 'strapline'; });
+				$$('article', doc).forEach(function(node) { node.className = 'node'; });
+				$$('.listingResultsWrapper > h2', doc).forEach(function(node) { node.parentNode.removeChild(node); });
+				$$('.listingResultsWrapper > a.viewMore', doc).forEach(function(node) { node.parentNode.removeChild(node); });
+				var content = $("#content", doc);
+				content.parentNode.removeChild(content);
+				doc.body.innerHTML = "";
+				doc.body.appendChild(content);
+			}
+		}
+	}
+});
+
+	
+})();
