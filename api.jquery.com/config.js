e48f4687edd6a1fa8303b5fc0217de835d548578
Sean Hogan <shogun70@gmail.com>
2013-01-10 23:54:53 +1100

    The api.jquery.com site was updated.

--- api.jquery.com/config.js
+++ api.jquery.com/config.js
@@ -10,10 +10,15 @@ Meeko.stuff.extend(Meeko.panner.options, {
 		"default": {
 			decor: "decor.html",
 			preprocess: function(doc, path) {
-				var content = $("#jq-primaryContent", doc);
+				Meeko.stuff.forEach($$("link[rel=stylesheet]", doc), function(link) {
+					link.parentNode.removeChild(link);
+				});
+				
+				var content = $("#content", doc);
 				content.parentNode.removeChild(content);
 				doc.body.innerHTML = "";
 				doc.body.appendChild(content);
+				if (path == '/') doc.body.className += " listing";
 			}
 		}
 	}
