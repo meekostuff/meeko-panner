7fc7483d1bd5a54c8e994fd9e7c0445dee232f61
Sean Hogan <shogun70@gmail.com>
2015-07-14 15:04:23 +1000

    Import TeXinfo config (new)

---
+++ generators/texinfo/README.md
@@ -0,0 +1,12 @@
+TexInfo
+=======
+
+<a id="sites"></a>
+Compatible Sites 
+----------------
+
+- [TeXinfo](http://www.gnu.org/software/texinfo/manual/texinfo/html_node/)
+- [GNU Make](http://www.gnu.org/software/make/manual/html_node/)
+- [GNU Bison](http://www.gnu.org/software/bison/manual/html_node/)
+- [Bash](http://http://www.gnu.org/software/bash/manual/html_node/)
+
---
+++ generators/texinfo/config.js
@@ -0,0 +1,43 @@
+(function() {
+
+var logger = Meeko.logger;
+var _ = Meeko.stuff;
+var DOM = Meeko.DOM;
+var URL = Meeko.URL, baseURL = URL(document.URL);
+
+var framesetURL = 'frameset.html'; // relative to config.js
+var scope;
+
+var genSigns = _.words('makeinfo texi2any');
+
+Meeko.framer.config({
+	lookup: function(url) {
+		if (!scope) return; // first time
+
+		// FIXME better notification for leaving doc-set
+		if (url.indexOf(scope) !== 0) return; 
+		return {
+			framesetURL: framesetURL,
+			scope: scope
+		}
+	},
+	
+	detect: function(doc) {
+		if (scope) return this.lookup(document.URL); // shouldn't be needed. lookup() will be valid
+	
+		var genLink = DOM.find('meta[name$=enerator]', doc.head);
+		var topLink = DOM.find('link[rel=start]', doc.head)
+		if (!genLink || !_.some(genSigns, function(gen) { return genLink.content.indexOf(gen) === 0; }) || !topLink) {
+			alert("This doesn't look like a supported TexInfo site. Sorry");
+			return;
+		}
+
+		var topHref = baseURL.resolve(topLink.getAttribute('href'));	
+		scope = URL(topHref).base; 
+
+		return this.lookup(document.URL);
+	}
+
+});
+	
+})();
