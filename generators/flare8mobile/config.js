1f35852f479aa554e3a3212b8ed341e60568e5da
Sean Hogan <shogun70@gmail.com>
2015-07-14 15:00:18 +1000

    Update flare8mobile to HyperFrameset including local copies of stylesheets and images

--- generators/flare8mobile/config.js
+++ generators/flare8mobile/config.js
@@ -1,25 +1,28 @@
 (function() {
 
-var _ = Meeko.stuff, forEach = _.forEach;
-var DOM = Meeko.DOM, $ = DOM.$, $$ = DOM.$$, $id = DOM.$id;
-var URL = DOM.URL;
+var _ = Meeko.stuff;
+var DOM = Meeko.DOM;
+var URL = Meeko.URL;
 
-var decorURL = "decor.html"; // relative to config.js
-var flareBase;
+var framesetURL = "frameset.html"; // relative to config.js
+var scope;
 
-Meeko.decor.config({
+Meeko.framer.config({
 	lookup: function(url) {
-		if (!flareBase) return; // first time
+		if (!scope) return; // first time
 
 		// FIXME better notification for leaving doc-set
-		if (url.indexOf(flareBase) !== 0) return; 
-		return decorURL;
+		if (url.indexOf(scope) !== 0) return; 
+		return {
+			framesetURL: framesetURL,
+			scope: scope
+		}
 	},
 	
 	detect: function(doc) {
-		if (flareBase) return; // shouldn't be needed. lookup() will be valid
+		if (scope) return this.lookup(document.URL); // shouldn't be needed. lookup() will be valid
 	
-		var homeButton = $('div#header > div#navigationContainer a.button', doc);
+		var homeButton = DOM.find('div#header > div#navigationContainer a.button', doc);
 	
 		if (!(homeButton && /Home/.test(homeButton.textContent || homeButton.innerText))) {
 			alert("This doesn't look like a supported Flare Mobile WebHelp site. Sorry");
@@ -28,38 +31,11 @@ Meeko.decor.config({
 	
 		var docURL = URL(document.URL);
 		var homeHref = docURL.resolve(homeButton.getAttribute('href'));
-		flareBase = URL(homeHref).base;
+		scope = URL(homeHref).base;
 	
-		return decorURL;
-	},
-	
-	normalize: function(doc) {
-		Meeko.decor.rebase(doc, flareBase);
+		return this.lookup(document.URL);
 	}
 
 });
-
-Meeko.panner.config({
-	normalize: function(doc, details) {
-		var oURL = URL(details.url);
-		var path = oURL.pathname;
-		forEach($$("script", doc), function(el) { el.parentNode.removeChild(el); })
-		forEach($$("link", doc), function(el) { if (/stylesheet/i.test(el.rel)) el.parentNode.removeChild(el); })
-
-		var html = $('html', doc); html.className = "left-layout";
-		
-		var breadcrumbs = $('.MCBreadcrumbsBox_Mobile_0', doc); if (breadcrumbs) breadcrumbs.className = "MCBreadcrumbsBox_0";
-
-		var content = doc.createElement('div');
-		content.id = "topic";
-		content.className = 'content';
-		
-		var node, topicBody = $('.MCTopicBody', doc);
-		while (node = topicBody.firstChild) content.appendChild(node);
-		doc.body.innerHTML = '';
-		doc.body.appendChild(content);
-	}
-});
-
 	
 })();
